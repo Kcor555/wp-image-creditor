388e58c9d6f4690143b01bb88871123e731247b6
Kris Cochran <74980761+Kcor555@users.noreply.github.com>
2022-06-08 01:27:07 -0400

    plugin creation

---
+++ wp-image-creditor.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Plugin Name: WP Image Creditor
+ * Description: Give credit to author or photographer
+ * Author: Kris Cochran
+ * Version: 1.0
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Copyright 2022 - All rights reserved.
+ */
